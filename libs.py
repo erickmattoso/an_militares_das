f96d5cd5de5d81c0aee93b11cbbd7b042df9126c
Ericson Mattoso <mattosoerick@gmail.com>
2020-01-12 09:29:18 -0300

    step 3

---
+++ libs.py
@@ -0,0 +1,19 @@
+# https://www1.folha.uol.com.br/poder/2019/01/militares-ja-se-espalham-por-21-areas-do-governo-bolsonaro-de-banco-estatal-a-educacao.shtml
+from math import pi
+from multiprocessing.pool import ThreadPool
+from urllib.request import urlretrieve
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import requests
+import shutil
+import urllib.request
+import zipfile
+pd.set_option('display.max_columns', 100)
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import googlemaps
+import pandas as pd
+import mplleaflet
