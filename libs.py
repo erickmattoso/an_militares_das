7a821999c9013b5b77dad9479912324eacb5565a
Ericson Mattoso <mattosoerick@gmail.com>
2020-01-13 19:53:09 -0300

    create map

--- libs.py
+++ libs.py
@@ -2,18 +2,18 @@
 from math import pi
 from multiprocessing.pool import ThreadPool
 from urllib.request import urlretrieve
+import folium
+import googlemaps
+import math
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
+import mplleaflet
 import numpy as np
 import os
 import pandas as pd
 import requests
+import seaborn as sns
 import shutil
 import urllib.request
 import zipfile
 pd.set_option('display.max_columns', 100)
-import pandas as pd
-import matplotlib.pyplot as plt
-import os
-import googlemaps
-import pandas as pd
-import mplleaflet
